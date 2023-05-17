87df5bea8b6f7314738871b07c9251ee948f0270
marianaarodrigues <marianaggl@gmail.com>
2023-05-17 15:23:17 -0300

    introdução a funções no JS

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function soma(n1, n2) {
+    alert(n1 + n2)
+}
+
+function subtracao(n1, n2) {
+    alert(n1 - n2)
+}
+
+function multiplicacao(n1, n2) {
+    alert(n1 * n2)
+}
+
+function divisao(n1, n2) {
+    alert(n1 / n2)
+}
+
+function mostrarnome() {
+    let nome = prompt("Qual o seu nome?")
+    alert("Olá, " + nome)
+}
+
+function arearetangulo(base, altura) {
+    alert("A área do triângulo é " + base * altura)
+}
